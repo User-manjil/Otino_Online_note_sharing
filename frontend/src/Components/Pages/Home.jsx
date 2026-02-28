7233e67e8868e6831f002cffeb028d602c592166
=Manjil <aryalmanzeel1@gmail.com>
2026-02-28 13:41:22 +0545

    feat : made card component

--- frontend/src/Components/Pages/Home.jsx
+++ frontend/src/Components/Pages/Home.jsx
@@ -1,9 +1,68 @@
 import React from 'react'
+import Title from '../reusable/Title'
+import Card from '../reusable/Card'
 
 const Home = () => {
   return (
-    <div className='w-full  px-10 h-screen  '>
-      <h1>this is home page</h1>
+    <div className='w-full   h-screen  '>
+      <div className='w-full h-90 bg-yellow-400 rounded-xl'></div>
+      <div className="flex flex-col">
+        <Title heading="Featured Notes" />
+        <div className="flex gap-10">
+           <Card 
+        title="Web Technolgy"
+        subCode="CSC-318"
+        author="Raju Rastogi"
+        rating="5"
+        rate="50"
+
+         />
+           <Card 
+        title="Web Technolgy"
+        subCode="CSC-318"
+        author="Raju Rastogi"
+        rating="5"
+        rate="50"
+
+         />
+           <Card 
+        title="Web Technolgy"
+        subCode="CSC-318"
+        author="Raju Rastogi"
+        rating="5"
+        rate="50"
+
+         />
+           <Card 
+        title="Web Technolgy"
+        subCode="CSC-318"
+        author="Raju Rastogi"
+        rating="5"
+        rate="50"
+
+         />
+           <Card 
+        title="Web Technolgy"
+        subCode="CSC-318"
+        author="Raju Rastogi"
+        rating="5"
+        rate="50"
+
+         />
+           <Card 
+        title="Web Technolgy"
+        subCode="CSC-318"
+        author="Raju Rastogi"
+        rating="5"
+        rate="50"
+
+         />
+        </div>
+       </div>
+       <div className="flex">
+        <Title heading="How it works" />
+       </div>
+
     </div>
   )
 }
---
+++ frontend/src/Components/reusable/Title.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Title = (props) => {
+  return (
+    <div className='w-full text-xl font-bold my-4 '>
+        <h1>{props.heading}</h1>
+      
+    </div>
+  )
+}
+
+export default Title
