be7ca9adc80fda07cc45dd0477d3879996b32f61
=Manjil <aryalmanzeel1@gmail.com>
2026-02-28 20:57:44 +0545

    added route

--- frontend/src/Components/Pages/BrowseNote.jsx
+++ frontend/src/Components/Pages/BrowseNote.jsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react'
 import Card from '../reusable/Card'
 
 const BrowseNote = () => {
+
+    
     const [searchdata, setSearchData] = useState('Web Technology')
   return (
     <div className='w-full flex flex-col gap-10'>
